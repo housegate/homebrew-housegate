5548f68f31b8eecb835a6c6f9c20c22a61f08d52
axiom147 <249228270+axiom147@users.noreply.github.com>
2026-08-14 07:09:22 +0000

    chore: update housegate formula to v0.8.1

--- Formula/housegate.rb
+++ Formula/housegate.rb
@@ -1,26 +1,25 @@
 class Housegate < Formula
   desc "ClickHouse native TCP proxy"
   homepage "https://github.com/housegate/housegate"
-  version "0.7.1"
   license "Apache-2.0"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/housegate/housegate/releases/download/v0.7.1/housegate-v0.7.1-darwin-arm64"
-    sha256 "a328988cdea7bacde9dda341253daccc8568e6f1628d17b02e81e4d3e59434a8"
+    url "https://github.com/housegate/housegate/releases/download/v0.8.1/housegate-v0.8.1-darwin-arm64"
+    sha256 "3307490a2146da1126db7051b6a05d5396f619ddc698b3a0b22aaad5bbdce78a"
   elsif OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/housegate/housegate/releases/download/v0.7.1/housegate-v0.7.1-linux-amd64"
-    sha256 "2e12b9cc4478df2d0bab71893be1316ce9919e2f5afb9fe593adfd9f8f90a9e9"
+    url "https://github.com/housegate/housegate/releases/download/v0.8.1/housegate-v0.8.1-linux-amd64"
+    sha256 "4021fdd1654e50e308139a1978670697f1a7c6a7a4e21a424a796044e57237f3"
   end
 
   def install
     if OS.mac?
-      bin.install "housegate-v0.7.1-darwin-arm64" => "housegate"
+      bin.install "housegate-v0.8.1-darwin-arm64" => "housegate"
     else
-      bin.install "housegate-v0.7.1-linux-amd64" => "housegate"
+      bin.install "housegate-v0.8.1-linux-amd64" => "housegate"
     end
   end
 
   test do
-    assert_match "housegate v0.7.1", shell_output("#{bin}/housegate --version")
+    assert_match "housegate v0.8.1", shell_output("#{bin}/housegate --version")
   end
 end
