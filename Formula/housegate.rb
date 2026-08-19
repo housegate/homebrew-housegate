40fc06a3bd3b417d443cff65b54ccaf183b0c881
axiom147 <249228270+axiom147@users.noreply.github.com>
2026-08-19 08:20:17 +0000

    chore: update housegate formula to v0.9.1

--- Formula/housegate.rb
+++ Formula/housegate.rb
@@ -4,22 +4,22 @@ class Housegate < Formula
   license "Apache-2.0"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/housegate/housegate/releases/download/v0.9.0/housegate-v0.9.0-darwin-arm64"
-    sha256 "2dcd498ff89e24d394266da9551b65716ae9360f81e88d391e737075b3e254b7"
+    url "https://github.com/housegate/housegate/releases/download/v0.9.1/housegate-v0.9.1-darwin-arm64"
+    sha256 "2e3a438eafc31e57ea3d5922d0cc72d3b708f70c1bf269eaafab32f36a44686d"
   elsif OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/housegate/housegate/releases/download/v0.9.0/housegate-v0.9.0-linux-amd64"
-    sha256 "5522a414a7e56cfd00dfe88460650613677e945d2968a4a56b6b0a19a9667046"
+    url "https://github.com/housegate/housegate/releases/download/v0.9.1/housegate-v0.9.1-linux-amd64"
+    sha256 "440cd511a86edb3e83d091bbf99865a7d1a2df3d4f21fdd47d2d206ffc61f754"
   end
 
   def install
     if OS.mac?
-      bin.install "housegate-v0.9.0-darwin-arm64" => "housegate"
+      bin.install "housegate-v0.9.1-darwin-arm64" => "housegate"
     else
-      bin.install "housegate-v0.9.0-linux-amd64" => "housegate"
+      bin.install "housegate-v0.9.1-linux-amd64" => "housegate"
     end
   end
 
   test do
-    assert_match "housegate v0.9.0", shell_output("#{bin}/housegate --version")
+    assert_match "housegate v0.9.1", shell_output("#{bin}/housegate --version")
   end
 end
